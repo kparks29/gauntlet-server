20d69f3c44de5aaeba4332a026c1d90d6bce9316
Kendrick Parks <parks.kendrick@gmail.com>
2016-11-08 18:17:09 -0800

    start of migration files

---
+++ server/db/drop.js
@@ -0,0 +1,18 @@
+import pgp from 'pg-promise'
+
+const pg = pgp()
+const db = pg(process.env.GS_DB_URL || '')
+
+db.tx(() => {
+	let queries = [
+		`DROP TABLE IF EXISTS users;`
+	]
+
+	return this.batch(queries.map((query) => {
+		return this.none(query, [])
+	}))
+}).then(() => {
+    console.log('Migration 1 Complete')
+}).catch((error) => {
+    console.log('Error migrating migration 1', error)
+})
--- server/db/migrations/migration-1.js
+++ server/db/migrations/migration-1.js
@@ -1,3 +1,22 @@
-import pg from 'pg-promise'
+import pgp from 'pg-promise'
 
+const pg = pgp()
 const db = pg(process.env.GS_DB_URL || '')
+
+let migration = db.tx(() => {
+	let queries = [
+		`CREATE TABLE users (
+			id SERIAL NOT NULL,
+			username TEXT,
+			salt UUID,
+			hashed_password UUID,
+			steam_id UUID
+		);`
+	]
+
+	return this.batch(queries.map((query) => {
+		return this.none(query, [])
+	}))
+})
+
+export default migration
